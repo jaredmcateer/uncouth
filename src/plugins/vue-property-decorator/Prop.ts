3113e95e967167f9f65a37da44f6499c4d829a3c
Jared McAteer <jared.mcateer@gmail.com>
2022-05-09 16:34:00 -0600

    feat: add PropType to props typedefs if possible

--- src/plugins/vue-property-decorator/Prop.ts
+++ src/plugins/vue-property-decorator/Prop.ts
@@ -1,5 +1,5 @@
 import { ASTConverter, ASTResultKind, ASTTransform, ASTResultToObject, ReferenceKind } from '../types'
-import type ts from 'typescript'
+import ts, { ObjectLiteralElementLike, ObjectLiteralExpression } from 'typescript'
 import { copySyntheticComments } from '../../utils'
 
 const propDecoratorName = 'Prop'
@@ -10,16 +10,40 @@ export const convertProp: ASTConverter<ts.PropertyDeclaration> = (node, options)
   }
   const decorator = node.decorators.find((el) => (el.expression as ts.CallExpression).expression.getText() === propDecoratorName)
   if (decorator) {
+    const propType = 'PropType'
     const tsModule = options.typescript
+    const type = node.type?.kind
     const decoratorArguments = (decorator.expression as ts.CallExpression).arguments
-    if (decoratorArguments.length > 0) {
+    const hasKnowableType = type !== undefined && type !== ts.SyntaxKind.AnyKeyword && type !== ts.SyntaxKind.UndefinedKeyword && type !== ts.SyntaxKind.UnknownKeyword
+    let addedPropKey = false
+
+    if (decoratorArguments.length > 0 || hasKnowableType) {
       const propName = node.name.getText()
-      const propArguments = decoratorArguments[0]
+      const propArguments = decoratorArguments[0] as ObjectLiteralExpression
+      const props = propArguments.properties
+        .reduce((accumulator, property) => {
+          if (property.kind === ts.SyntaxKind.PropertyAssignment) {
+            if (node.type && property.name.getText() === 'type') {
+              const typeReference = ts.createTypeReferenceNode(propType, [node.type])
+              property.initializer = tsModule.createAsExpression(property.initializer, typeReference)
+              addedPropKey = true
+            }
+
+            accumulator.push(property)
+          }
+
+          return accumulator
+        }, [] as ObjectLiteralElementLike[])
+
+      const args = tsModule.createObjectLiteral(props)
 
       return {
         tag: 'Prop',
-        kind: ASTResultKind.OBJECT,
-        imports: [],
+        kind: ASTResultKind.COMPOSITION,
+        imports: addedPropKey ? [{
+          named: [propType],
+          external: (options.compatible) ? '@vue/composition-api' : 'vue'
+        }] : [],
         reference: ReferenceKind.PROPS,
         attributes: [propName],
         nodes: [
@@ -27,7 +51,7 @@ export const convertProp: ASTConverter<ts.PropertyDeclaration> = (node, options)
             tsModule,
             tsModule.createPropertyAssignment(
               tsModule.createIdentifier(propName),
-              propArguments
+              args
             ),
             node
           )
@@ -38,6 +62,7 @@ export const convertProp: ASTConverter<ts.PropertyDeclaration> = (node, options)
 
   return false
 }
+
 export const mergeProps: ASTTransform = (astResults, options) => {
   const tsModule = options.typescript
   const propTags = ['Prop', 'Model']
@@ -45,11 +70,12 @@ export const mergeProps: ASTTransform = (astResults, options) => {
   const propASTResults = astResults.filter((el) => propTags.includes(el.tag))
   const otherASTResults = astResults.filter((el) => !propTags.includes(el.tag))
   const modelASTResult = astResults.find((el) => el.tag === 'Model')
+  const imports = propASTResults.find(el => el.imports.length > 0)?.imports || [];
 
   const mergeASTResult: ASTResultToObject = {
     tag: 'Prop',
     kind: ASTResultKind.OBJECT,
-    imports: [],
+    imports,
     reference: ReferenceKind.PROPS,
     attributes: propASTResults.map((el) => el.attributes).reduce((array, el) => array.concat(el), []),
     nodes: [
