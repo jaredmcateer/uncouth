717e3be205012ed0c9b6a67b214aa0e811f505bb
Jared McAteer <jared.mcateer@gmail.com>
2022-05-13 19:40:41 -0600

    refactor(format): change format to use just prettier

--- src/format.ts
+++ src/format.ts
@@ -1,70 +1,26 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { UncouthOptions } from "./options";
-import path from "path";
 import { log } from "./debug";
-import prettier from "prettier/standalone";
+import prettier from "prettier";
 import prettierTypescriptParser from "prettier/parser-typescript";
-import { existsFileSync } from "./file";
+import { readFileSync } from "fs";
 
 export function format(content: string, options: UncouthOptions): string {
-  const isNode = typeof window === "undefined";
-  if (!isNode) {
-    return prettier.format(content, {
-      plugins: [prettierTypescriptParser],
-      parser: "typescript",
-      semi: false,
-      singleQuote: true,
-    });
+  const configFile = options.prettierConfig ?? "";
+  let config: prettier.Options = {
+    plugins: [prettierTypescriptParser],
+    parser: "typescript",
+  };
+
+  try {
+    const text = readFileSync(configFile, "utf-8");
+    const tmpConfig = prettier.resolveConfig.sync(text);
+    if (!tmpConfig) throw new Error("File not found");
+    config = tmpConfig;
+  } catch (err) {
+    log(`Error: invalid prettier configuration, falling back to default config... (error ${err}`);
   }
 
-  const eslintConfigPath = path.resolve(options.root, options.eslintConfigFile);
-  const prettierFormat = require("prettier-eslint") as (config: unknown) => string;
-  const prettierEslintOpions = existsFileSync(eslintConfigPath)
-    ? {
-        text: content,
-        filePath: eslintConfigPath,
-        prettierOptions: {
-          parser: "typescript",
-        },
-        fallbackPrettierOptions: {
-          parser: "typescript",
-        },
-      }
-    : {
-        text: content,
-        filePath: "",
-        eslintConfig: {
-          parser: require.resolve("@typescript-eslint/parser"),
-          parserOptions: {
-            sourceType: "module",
-            ecmaFeatures: {
-              jsx: false,
-            },
-          },
-          rules: {
-            semi: ["error", "never"],
-            "padding-line-between-statements": [
-              "error",
-              { blankLine: "always", prev: "*", next: "export" },
-              { blankLine: "always", prev: "const", next: "*" },
-              { blankLine: "always", prev: "*", next: "const" },
-            ],
-          },
-        },
-        prettierOptions: {
-          parser: "typescript",
-          Semicolons: false,
-          singleQuote: true,
-          trailingComma: "none",
-        },
-        fallbackPrettierOptions: {
-          parser: "typescript",
-          singleQuote: true,
-          Semicolons: false,
-          trailingComma: "none",
-        },
-      };
-
-  log("Format result code.....");
-  return prettierFormat(prettierEslintOpions);
+  log("Formatting code...");
+  return prettier.format(content, config);
 }
