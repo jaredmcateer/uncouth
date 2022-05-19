cde8e5e2f72332387e1f05fdc268c1e3e622837d
Jared McAteer <jared.mcateer@gmail.com>
2022-05-19 15:49:40 -0600

    fix(vuex-class): fix getter property access

--- src/plugins/vuex-class/Getter.ts
+++ src/plugins/vuex-class/Getter.ts
@@ -4,7 +4,6 @@ import { isValidIdentifier } from "../../utils";
 import { ASTConverter, ASTResultKind, ReferenceKind } from "../types";
 
 const GETTER_DECORATOR = "Getter";
-const u = undefined;
 
 let $t: TsHelper;
 
@@ -12,7 +11,7 @@ let $t: TsHelper;
  * @example
  * \@Getter('myNamespace/someGetter') getSomething: boolean;
  * // converts to
- * const getSomething = computer(() => {
+ * const getSomething = computed<boolean>(() => {
  *  return store.getter('myNamespace/someGetter');
  * })
  */
@@ -26,14 +25,14 @@ export const convertVuexGetter: ASTConverter<ts.PropertyDeclaration> = (node, op
   const decoratorArgs = (decorator.expression as ts.CallExpression).arguments;
   const getterPath = decoratorArgs.length === 1 ? decoratorArgs[0].getText() : computedGetterName;
   const getterAccessExpr = getAccessExpression(getterPath);
-  const getterArrowFunc = [createArrowFunction(getterAccessExpr)];
+  const getterArrowFunc = [$t.createSimpleArrowFunction(getterAccessExpr)];
   const typeArgs = node.type ? [node.type] : [];
   const computedCallExpr = $t.createCallExpression("computed", typeArgs, getterArrowFunc);
   const constStatement = $t.createConstStatement(computedGetterName, computedCallExpr);
   const computedGetterStatement = $t.copySyntheticComments(constStatement, node);
 
   return {
-    tag: "Computed",
+    tag: "Computed-Vuex-Getter",
     kind: ASTResultKind.COMPOSITION,
     imports: [...$t.namedImports(["useStore"], "vuex"), ...$t.namedImports(["computed"])],
     composables: [{ default: options.vuexKey, func: "useStore" }],
@@ -43,20 +42,10 @@ export const convertVuexGetter: ASTConverter<ts.PropertyDeclaration> = (node, op
   };
 };
 
-function createArrowFunction(
-  getterAccessExpr: ts.ElementAccessExpression | ts.PropertyAccessExpression
-) {
-  const _return = $t.factory.createReturnStatement(getterAccessExpr);
-  const _body = $t.factory.createBlock([_return]);
-  const arrowFunc = $t.factory.createArrowFunction(u, u, [], u, $t.rocketToken, _body);
-
-  return arrowFunc;
-}
-
 function getAccessExpression(id: string) {
   const _id = $t.factory.createIdentifier(id);
   const _store = $t.factory.createIdentifier("store");
-  const _dispatch = $t.factory.createIdentifier("dispatch");
+  const _dispatch = $t.factory.createIdentifier("getters");
   const _accessorExpr = $t.factory.createPropertyAccessExpression(_store, _dispatch);
 
   // 'myNamespace/foo' needs bracket notation to access on getter
