14aa0131fd533cf84974ebc773b2b41990076362
Jared McAteer <jared.mcateer@gmail.com>
2022-05-26 16:23:55 -0600

    fix(prop-decorator): try to use ts type when building props

--- src/plugins/vue-property-decorator/Prop.ts
+++ src/plugins/vue-property-decorator/Prop.ts
@@ -9,6 +9,7 @@ import ts from "typescript";
 import { TsHelper } from "../../helpers/TsHelper";
 
 const propDecoratorName = "Prop";
+let $t: TsHelper;
 
 export const convertProp: ASTConverter<ts.PropertyDeclaration> = (node, options) => {
   if (!node.decorators) {
@@ -19,8 +20,8 @@ export const convertProp: ASTConverter<ts.PropertyDeclaration> = (node, options)
   );
   if (decorator) {
     const propType = "PropType";
-    const $t = new TsHelper(options);
-    const type = node.type?.kind;
+    $t = new TsHelper(options);
+    let type = node.type?.kind;
     const decoratorArguments = (decorator.expression as ts.CallExpression).arguments;
     const hasKnowableType =
       type !== undefined &&
@@ -28,11 +29,13 @@ export const convertProp: ASTConverter<ts.PropertyDeclaration> = (node, options)
       type !== ts.SyntaxKind.UndefinedKeyword &&
       type !== ts.SyntaxKind.UnknownKeyword;
     let addedPropKey = false;
+    let addTodoComplex = false;
+
+    let props: ts.ObjectLiteralElementLike[] = [];
+    const propName = node.name.getText();
 
     if (decoratorArguments.length > 0 || hasKnowableType) {
-      const propName = node.name.getText();
       const propArguments = decoratorArguments[0];
-      let props: ts.ObjectLiteralElementLike[] = [];
       if (propArguments && $t.module.isObjectLiteralExpression(propArguments)) {
         props = propArguments.properties.reduce((accumulator, property) => {
           if (property.kind === ts.SyntaxKind.PropertyAssignment) {
@@ -49,25 +52,92 @@ export const convertProp: ASTConverter<ts.PropertyDeclaration> = (node, options)
 
           return accumulator;
         }, [] as ts.ObjectLiteralElementLike[]);
+      } else if (node.type && hasKnowableType) {
+        let primType: string | undefined;
+
+        if ($t.module.isLiteralTypeNode(node.type)) {
+          type = node.type.literal.kind;
+        }
+
+        let addTsType = false;
+
+        switch (type) {
+          case ts.SyntaxKind.StringLiteral:
+            addTsType = true;
+            primType = "String";
+            break;
+          case ts.SyntaxKind.StringKeyword:
+            primType = "String";
+            break;
+          case ts.SyntaxKind.TrueKeyword:
+          case ts.SyntaxKind.FalseKeyword:
+            addTsType = true;
+            primType = "Boolean";
+            break;
+          case ts.SyntaxKind.BooleanKeyword:
+            primType = "Boolean";
+            break;
+          case ts.SyntaxKind.NumericLiteral:
+            addTsType = true;
+            primType = "Number";
+            break;
+          case ts.SyntaxKind.NumberKeyword:
+            primType = "Number";
+            break;
+          default:
+            addTodoComplex = true;
+            break;
+        }
+
+        if (primType) {
+          const vueIdentifier = $t.factory.createIdentifier(primType);
+          const tsIdentifier = addTsType
+            ? $t.factory.createIdentifier(node.type.getText())
+            : undefined;
+          props = [createPropTypeAssignment(vueIdentifier, tsIdentifier)];
+        }
       }
-
-      const args = $t.factory.createObjectLiteralExpression(props);
-
-      return {
-        tag: "Prop",
-        kind: ASTResultKind.COMPOSITION,
-        imports: addedPropKey ? $t.namedImports([propType]) : [],
-        reference: ReferenceKind.PROPS,
-        attributes: [propName],
-        nodes: [
-          $t.copySyntheticComments($t.factory.createPropertyAssignment(propName, args), node),
-        ],
-      };
     }
+    const args = $t.factory.createObjectLiteralExpression(props.length > 0 ? props : undefined);
+    let propAssignment = $t.factory.createPropertyAssignment(propName, args);
+    propAssignment = addTodoComplex
+      ? $t.addTodoComment(
+          propAssignment,
+          `Too complex to determine primitive type: ${node.type!.getText()} `,
+          true
+        )
+      : propAssignment;
+
+    return {
+      tag: "Prop",
+      kind: ASTResultKind.COMPOSITION,
+      imports: addedPropKey ? $t.namedImports([propType]) : [],
+      reference: ReferenceKind.PROPS,
+      attributes: [propName],
+      nodes: [$t.copySyntheticComments(propAssignment, node)],
+    };
   }
 
   return false;
 };
+
+function createPropTypeAssignment(
+  vueTypeIdentifier: ts.Identifier,
+  tsTypeIdentifier?: ts.Identifier
+) {
+  const typeId = $t.factory.createIdentifier("type");
+
+  if (!tsTypeIdentifier) {
+    return $t.factory.createPropertyAssignment(typeId, vueTypeIdentifier);
+  }
+
+  const tsTypeRef = $t.factory.createTypeReferenceNode(tsTypeIdentifier, undefined);
+  const propTypeId = $t.factory.createIdentifier("PropType");
+  const propTypeRef = $t.factory.createTypeReferenceNode(propTypeId, [tsTypeRef]);
+  const asExpr = $t.factory.createAsExpression(vueTypeIdentifier, propTypeRef);
+  const propAssignment = $t.factory.createPropertyAssignment(typeId, asExpr);
+  return propAssignment;
+}
 
 export const mergeProps: ASTTransform = (astResults, options) => {
   const $t = new TsHelper(options);
